7ed952fb25c1c35a94f98a5b8e191678bba8246d
Renuka-12324 <renukachama662002@gmail.com>
2025-01-01 19:11:34 +0530

    Add files via upload

---
+++ Dev.java
@@ -0,0 +1,11 @@
+package com.example.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Working on project");
+    }
+
+}
